37a61dbbd91facaf82109a1d91b69644644085f1
petrus <petrus.jonsson@gmail.com>
2017-01-11 14:36:59 +0100

    Add proper system info page

--- ESSArch_TA/frontend/static/frontend/scripts/controllers/UtilCtrl.js
+++ ESSArch_TA/frontend/static/frontend/scripts/controllers/UtilCtrl.js
@@ -1,8 +1,19 @@
-angular.module('myApp').controller('UtilCtrl', function($scope, $state, $location, $window) {
+angular.module('myApp').controller('UtilCtrl', function($scope, $state, $location, $window, $http, appConfig) {
     $scope.reloadPage = function (){
         $state.reload();
     }
     $scope.redirectAdmin = function () {
         $window.location.href="/admin/";
     }
+    $scope.getVersionInfo = function() {
+        $http({
+            method: 'GET',
+            url: appConfig.djangoUrl+"sysinfo"
+        }).then(function(response){
+            $scope.sysInfo = response.data;
+        }, function() {
+            console.log('error');
+        })
+    }
+    $scope.getVersionInfo();
 });
