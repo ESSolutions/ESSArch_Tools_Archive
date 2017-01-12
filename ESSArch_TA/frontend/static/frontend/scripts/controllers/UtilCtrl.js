0e9a47cd486185ea9e5a492ac162a49ba94387e8
petrus <petrus.jonsson@gmail.com>
2017-01-12 13:30:06 +0100

    Remove duplicate code

--- ESSArch_TA/frontend/static/frontend/scripts/controllers/UtilCtrl.js
+++ ESSArch_TA/frontend/static/frontend/scripts/controllers/UtilCtrl.js
@@ -5,15 +5,4 @@ angular.module('myApp').controller('UtilCtrl', function($scope, $state, $locatio
     $scope.redirectAdmin = function () {
         $window.location.href="/admin/";
     }
-    $scope.getVersionInfo = function() {
-        $http({
-            method: 'GET',
-            url: appConfig.djangoUrl+"sysinfo/"
-        }).then(function(response){
-            $scope.sysInfo = response.data;
-        }, function() {
-            console.log('error');
-        })
-    }
-    $scope.getVersionInfo();
 });
