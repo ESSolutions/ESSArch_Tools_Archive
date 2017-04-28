5584270c2e34d4a622563bf993eb3524b6ea43e3
Oskar Persson <oskar.persson@polken.se>
2017-04-28 11:04:10 +0200

    Remove lodash from gulpfile

--- ESSArch_TA/frontend/static/frontend/gulpfile.js
+++ ESSArch_TA/frontend/static/frontend/gulpfile.js
@@ -66,7 +66,6 @@ var vendorFiles = [
         'scripts/bower_components/bootstrap/dist/js/bootstrap.js',
         'scripts/bower_components/angular-moment-picker/dist/angular-moment-picker.js',
         'scripts/bower_components/angular-link-header-parser/release/angular-link-header-parser.js',
-        'scripts/bower_components/lodash/lodash.js',
         'scripts/bower_components/uri-util/dist/uri-util.js',
         'scripts/bower_components/marked/lib/marked.js',
         'scripts/bower_components/angular-marked/dist/angular-marked.js',
