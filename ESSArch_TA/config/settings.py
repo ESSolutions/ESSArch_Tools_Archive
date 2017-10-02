ea70903e08f516012d34424e5b9e02b535668407
Oskar Persson <oskar.persson@polken.se>
2017-10-02 15:45:23 +0200

    Add default logging config

--- ESSArch_TA/config/settings.py
+++ ESSArch_TA/config/settings.py
@@ -165,6 +165,25 @@ CACHES = {
     }
 }
 
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'core': {
+            'level': 'DEBUG',
+            'class': 'ESSArch_Core.log.dbhandler.DBHandler',
+            'application': 'ESSArch Tools for Archive',
+        }
+    },
+    'loggers': {
+        'essarch': {
+            'handlers': ['core'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
