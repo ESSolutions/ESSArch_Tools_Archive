aa4ae8824ddcdb383447b4d8e97afc7c8a220fc3
petrus <petrus.jonsson@gmail.com>
2017-10-27 16:29:04 +0200

    Add list view for filebrowser with switch between list and grid

--- ESSArch_TA/frontend/static/frontend/scripts/controllers/FilebrowserController.js
+++ ESSArch_TA/frontend/static/frontend/scripts/controllers/FilebrowserController.js
@@ -1,6 +1,18 @@
 angular.module('myApp').controller('FilebrowserController', function ($scope, $window, $sce, $rootScope, appConfig, listViewService, $uibModal) {
     $scope.previousGridArrays = [];
     $scope.ip = $rootScope.ip;
+    $scope.listView = false;
+    $scope.gridView = true;
+    $scope.useListView = function() {
+        $scope.listView = true;
+        $scope.gridView = false;
+    }
+
+    $scope.useGridView = function() {
+        $scope.listView = false;
+        $scope.gridView = true;
+    }
+
     $scope.previousGridArraysString = function () {
         var retString = "";
         $scope.previousGridArrays.forEach(function (card) {
