9dbff796e34d6960d1a0413d0bd64701754bcfb7
petrus <petrus.jonsson@gmail.com>
2017-11-27 14:49:54 +0100

    Add check to prevent initial error

--- ESSArch_TA/frontend/static/frontend/scripts/controllers/ImportCtrl.js
+++ ESSArch_TA/frontend/static/frontend/scripts/controllers/ImportCtrl.js
@@ -94,11 +94,15 @@ angular.module('myApp').controller('ImportCtrl', function($q, $rootScope, $scope
     }
 
     $scope.$watch(function(){return vm.saFromFile}, function() {
-        vm.addSaFromFile(vm.saFromFile);
+        if(vm.saFromFile) {
+            vm.addSaFromFile(vm.saFromFile);
+        }
     });
 
     $scope.$watch(function(){return vm.profileFromFile}, function() {
-        vm.addProfileFromFile(vm.profileFromFile);
+        if(vm.profileFromFile){
+            vm.addProfileFromFile(vm.profileFromFile);
+        }
     });
 
     function saProfileExistsModal(profile) {
