3d030997c61cf125fdeef76d6b4600524060aa63
petrus <petrus.jonsson@gmail.com>
2017-12-14 15:43:53 +0100

    Add highlight for validationReports element and split formatXml to several functions

--- ESSArch_TA/frontend/static/frontend/scripts/services/workareaValidation.js
+++ ESSArch_TA/frontend/static/frontend/scripts/services/workareaValidation.js
@@ -1,22 +1,59 @@
 angular.module('myApp').factory('WorkareaValidation', function ($rootScope, $q, appConfig, $http, $sce, $filter, myService) {
     var service = {};
 
+    /**
+     * Format and highligt XML string from validation message
+     * @param {Object} validation
+     */
     function formatXml(validation) {
+        // General formatting of XML string
         var pretty = $filter("prettyXml")(validation.message);
         pretty = myService.replaceAll(pretty, "<", "&lt;"); //Replace open tags with "<" character
         pretty = myService.replaceAll(pretty, ">", "&gt;"); //Replace end tags with ">" character
-        // Mediaconch validation message highlighing
-        if(validation.validator == "MediaconchValidator") {
-            pretty = myService.replaceAll(pretty, 'outcome="fail"', '<span style="background-color: red; color: white; font-weight: bold;">outcome="fail"</span>')
-            pretty = myService.replaceAll(pretty, 'outcome="pass"', '<span style="background-color: green; color: white; font-weight: bold;">outcome="pass"</span>')
+        if (validation.validator == "MediaconchValidator") {
+            pretty = mediaconchValidator(pretty);
         }
-        // VeraPDF validation message highlighting
-        if(validation.validator == "VeraPDFValidator") {
-            pretty = myService.replaceAll(pretty, 'isCompliant="false"', '<span style="background-color: red; color: white; font-weight: bold;">isCompliant="false"</span>')
-            pretty = myService.replaceAll(pretty, 'isCompliant="true"', '<span style="background-color: green; color: white; font-weight: bold;">isCompliant="true"</span>')
-            pretty = pretty.replace(/passedChecks="([\d]+)" failedChecks="([^0])"/g, '<span style="background-color: red; color: white;">passedChecks="$1" failedChecks="$2"</span>')
-            pretty = pretty.replace(/passedChecks="([\d]+)" failedChecks="0"/g, '<span style="background-color: green; color: white;">passedChecks="$1" failedChecks="0"</span>')
-            pretty = pretty.replace(/&lt;exceptionMessage&gt;([\s\S]*?)&lt;\/exceptionMessage\&gt;/g, '<span style="background-color: red; color: white;">&lt;exceptionMessage&gt;$1&lt;/exceptionMessage&gt;</span>')
+        if (validation.validator == "VeraPDFValidator") {
+            pretty = veraPDFValidator(pretty);
+        }
+        return pretty;
+    }
+
+    /**
+     * Highlight mediaconch result
+     * @param {String} pretty
+     */
+    function mediaconchValidator(pretty) {
+        pretty = myService.replaceAll(pretty, 'outcome="fail"', '<span style="background-color: red; color: white; font-weight: bold;">outcome="fail"</span>')
+        pretty = myService.replaceAll(pretty, 'outcome="pass"', '<span style="background-color: green; color: white; font-weight: bold;">outcome="pass"</span>')
+        return pretty;
+    }
+
+    /**
+     * Highlight VeraPDF result
+     * @param {String} pretty
+     */
+    function veraPDFValidator(pretty) {
+        // Mark isCompliant true/false
+        pretty = myService.replaceAll(pretty, 'isCompliant="false"', '<span style="background-color: red; color: white; font-weight: bold;">isCompliant="false"</span>')
+        pretty = myService.replaceAll(pretty, 'isCompliant="true"', '<span style="background-color: green; color: white; font-weight: bold;">isCompliant="true"</span>')
+
+        // Mark passedChecks and failedChecks
+        pretty = pretty.replace(/passedChecks="([\d]+)" failedChecks="([^0])"/g, '<span style="background-color: red; color: white;">passedChecks="$1" failedChecks="$2"</span>')
+        pretty = pretty.replace(/passedChecks="([\d]+)" failedChecks="0"/g, '<span style="background-color: green; color: white;">passedChecks="$1" failedChecks="0"</span>')
+
+        // Mark exceptionMessage element if there is one
+        pretty = pretty.replace(/&lt;exceptionMessage&gt;([\s\S]*?)&lt;\/exceptionMessage\&gt;/g, '<span style="background-color: red; color: white;">&lt;exceptionMessage&gt;$1&lt;/exceptionMessage&gt;</span>')
+
+        // Mark validationReports element
+        var regex = /&lt;validationReports compliant="([\d])" nonCompliant="([\d])" failedJobs="([\d])"&gt;([\s\S]*?)&lt;\/validationReports&gt;/g;
+        var match = regex.exec(pretty);
+        if (match[2] != "0" || match[3] != "0") {
+            var replace = '<span style="background-color: red; color: white;">&lt;validationReports compliant="$1" nonCompliant="$2" failedJobs="$3"&gt;$4&lt;\/validationReports&gt;</span>';
+            pretty = pretty.replace(regex, replace);
+        } else {
+            var replace = '<span style="background-color: green; color: white;">&lt;validationReports compliant="$1" nonCompliant="$2" failedJobs="$3"&gt;$4&lt;\/validationReports&gt;</span>';
+            pretty = pretty.replace(regex, replace)
         }
         return pretty;
     }
