c7d0081869951473131b381b58a57a3ed9951845
Oskar Persson <oskar.persson@polken.se>
2018-01-15 19:21:57 +0100

    Add url for docs

--- ESSArch_TA/config/settings.py
+++ ESSArch_TA/config/settings.py
@@ -84,6 +84,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'frontend',
     'ESSArch_Core.auth',
+    'ESSArch_Core.docs',
     'ESSArch_Core.configuration',
     'ESSArch_Core.ip',
     'ESSArch_Core.profiles',
@@ -282,6 +283,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Documentation
+DOCS_ROOT = os.path.join(BASE_DIR, 'docs/_build/html')
 
 # Add etp vhost to rabbitmq:
 # rabbitmqctl add_user guest guest
