40d009d8cbb6b122cfe65eeaea7dac1ae4930f9e
Oskar Persson <oskar.persson@polken.se>
2018-02-07 15:21:01 +0100

    Update default install script to use groups manager

--- ESSArch_TA/install/install_default_config_eta.py
+++ ESSArch_TA/install/install_default_config_eta.py
@@ -27,7 +27,8 @@
 import django
 django.setup()
 
-from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth.models import Permission
+from groups_manager.models import Group, Member, GroupType
 from django.contrib.contenttypes.models import ContentType
 
 from ESSArch_Core.configuration.models import Parameter, Path, Agent
@@ -44,48 +45,13 @@ def installDefaultConfiguration():
 
 
 def installDefaultUsers():
-    user_superuser, created = User.objects.get_or_create(
-        username='superuser', email='superuser@essolutions.se',
-        is_staff=True, is_superuser=True
-    )
-    if created:
-        user_superuser.set_password('superuser')
-        user_superuser.save()
 
-    user_user, _ = User.objects.get_or_create(
-        username='user', email='usr1@essolutions.se'
-    )
-    user_user.set_password('user')
-    user_user.save()
+    #####################################
+    # Groups and permissions
+    organization, _ = GroupType.objects.get_or_create(label="organization")
+    default_org, _ = Group.objects.get_or_create(name='Default', group_type=organization)
 
-    user_admin, _ = User.objects.get_or_create(
-        username='admin', email='admin@essolutions.se',
-        is_staff=True
-    )
-    user_admin.set_password('admin')
-    user_admin.save()
-
-    user_sysadmin, _ = User.objects.get_or_create(
-        username='sysadmin', email='sysadmin@essolutions.se',
-        is_staff=True
-    )
-    user_sysadmin.set_password('sysadmin')
-    user_sysadmin.save()
-
-    user_reta, _ = User.objects.get_or_create(
-        username='reta', email='reta@essolutions.se',
-    )
-    user_reta.set_password('reta')
-    user_reta.save()
-
-    group_user, _ = Group.objects.get_or_create(name='user')
-    group_admin, _ = Group.objects.get_or_create(name='admin')
-    group_sysadmin, _ = Group.objects.get_or_create(name='sysadmin')
-
-    content_type = ContentType.objects.get_for_model(InformationPackage)
-    can_receive_remote_files = Permission.objects.get(content_type=content_type, codename='can_receive_remote_files')
-    user_reta.user_permissions.add(can_receive_remote_files)
-
+    group_user, _ = Group.objects.get_or_create(name='user', parent=default_org)
     permission_list_user = [
         ## ---- app: ip ---- model: informationpackage
         ['receive','ip','informationpackage'],                    # Can receive IP
@@ -100,24 +66,26 @@ def installDefaultUsers():
                                           codename=p[0], content_type__app_label=p[1],
                                           content_type__model=p[2],
                                           )
-        group_user.permissions.add(p_obj)
+        group_user.django_group.permissions.add(p_obj)
 
+    group_admin, _ = Group.objects.get_or_create(name='admin', parent=default_org)
     permission_list_admin = [
         ## ---- app: ip ---- model: informationpackage
         ['receive','ip','informationpackage'],                    # Can receive IP
         ['transfer_sip','ip','informationpackage'],                    # Can transfer SIP
         ## ---- app: WorkflowEngine ---- model: processtask
         #['can_undo','WorkflowEngine','processtask'],             # Can undo tasks (other)
-        #['can_retry','WorkflowEngine','processtask'],             # Can retry tasks (other)                             
+        #['can_retry','WorkflowEngine','processtask'],             # Can retry tasks (other)
     ]
-   
+
     for p in permission_list_admin:
         p_obj = Permission.objects.get(
                                           codename=p[0], content_type__app_label=p[1],
                                           content_type__model=p[2],
-                                          )        
-        group_admin.permissions.add(p_obj)
+                                          )
+        group_admin.django_group.permissions.add(p_obj)
 
+    group_sysadmin, _ = Group.objects.get_or_create(name='sysadmin', parent=default_org)
     permission_list_sysadmin = [
         ## ---- app: auth ---- model: group
         ['add_group','auth','group'],                    # Can add group
@@ -138,7 +106,7 @@ def installDefaultUsers():
         ## ---- app: configuration ---- model: eventtype
         ['add_eventtype','configuration','eventtype'],                    # Can add eventtype
         ['change_eventtype','configuration','eventtype'],                    # Can change eventtype
-        ['delete_eventtype','configuration','eventtype'],                    # Can delete eventtype        
+        ['delete_eventtype','configuration','eventtype'],                    # Can delete eventtype
         ## ---- app: profiles ---- model: profile
         ['add_profile','profiles','profile'],                    # Can add profile
         ['change_profile','profiles','profile'],                    # Can change profile
@@ -146,20 +114,56 @@ def installDefaultUsers():
         ## ---- app: profiles ---- model: submissionagreement
         ['add_submissionagreement','profiles','submissionagreement'],                    # Can add submissionagreement
         ['change_submissionagreement','profiles','submissionagreement'],                    # Can change submissionagreement
-        ['delete_submissionagreement','profiles','submissionagreement'],                    # Can delete submissionagreement        
+        ['delete_submissionagreement','profiles','submissionagreement'],                    # Can delete submissionagreement
     ]
-   
+
     for p in permission_list_sysadmin:
         p_obj = Permission.objects.get(
                                           codename=p[0], content_type__app_label=p[1],
                                           content_type__model=p[2],
-                                          )        
-        group_sysadmin.permissions.add(p_obj)
+                                          )
+        group_sysadmin.django_group.permissions.add(p_obj)
 
-    group_user.user_set.add(user_user)
-    group_user.user_set.add(user_reta)
-    group_admin.user_set.add(user_admin)
-    group_sysadmin.user_set.add(user_sysadmin)
+    #####################################
+    # Users
+    user_superuser, created = Member.objects.get_or_create(
+        first_name='superuser', last_name='Lastname',
+        username='superuser', email='superuser@essolutions.se',
+    )
+    if created:
+        user_superuser.django_user.set_password('superuser')
+        user_superuser.django_user.is_staff=True
+        user_superuser.django_user.is_superuser=True
+        user_superuser.django_user.save()
+
+    user_user, created = Member.objects.get_or_create(
+        first_name='user', last_name='Lastname',
+        username='user', email='user@essolutions.se'
+    )
+    if created:
+        user_user.django_user.set_password('user')
+        user_user.django_user.save()
+        group_user.add_member(user_user)
+
+    user_admin, created = Member.objects.get_or_create(
+        first_name='admin', last_name='Lastname',
+        username='admin', email='admin@essolutions.se',
+    )
+    if created:
+        user_admin.django_user.set_password('admin')
+        user_admin.django_user.is_staff=True
+        user_admin.django_user.save()
+        group_admin.add_member(user_admin)
+
+    user_sysadmin, created = Member.objects.get_or_create(
+        first_name='sysadmin', last_name='Lastname',
+        username='sysadmin', email='sysadmin@essolutions.se',
+    )
+    if created:
+        user_sysadmin.django_user.set_password('sysadmin')
+        user_sysadmin.django_user.is_staff=True
+        user_sysadmin.django_user.save()
+        group_sysadmin.add_member(user_sysadmin)
 
     return 0
 
